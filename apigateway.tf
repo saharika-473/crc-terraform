2de5fe4c35ee8a450cc2c974a3c85285794185a7
Rahul Patel <patelr4142@gmail.com>
2024-02-02 00:51:06 +0530

    added get method for visit counter

--- apigateway.tf
+++ apigateway.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_resource" "countVisitor" {
   parent_id   = aws_api_gateway_rest_api.CloudResumeChallengeAPI.root_resource_id
   path_part   = "countVisitor"
 }
+
+resource "aws_api_gateway_method" "GETcountVisitor" {
+  rest_api_id   = aws_api_gateway_rest_api.CloudResumeChallengeAPI.id
+  resource_id   = aws_api_gateway_resource.countVisitor.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
