0fb02c5fc70abd7fe3e8bca41a3643132afbf967
Rahul Patel <patelr4142@gmail.com>
2024-02-02 00:30:35 +0530

    added countvisitor resource to api

--- apigateway.tf
+++ apigateway.tf
@@ -2,3 +2,9 @@ resource "aws_api_gateway_rest_api" "CloudResumeChallengeAPI" {
   name        = "${local.naming_convention}-API"
   description = "API for Cloud Resume Challenge"
 }
+
+resource "aws_api_gateway_resource" "countVisitor" {
+  rest_api_id = aws_api_gateway_rest_api.CloudResumeChallengeAPI.id
+  parent_id   = aws_api_gateway_rest_api.CloudResumeChallengeAPI.root_resource_id
+  path_part   = "countVisitor"
+}
