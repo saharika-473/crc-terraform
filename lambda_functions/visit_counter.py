55ac6a622f681d4574d43e10671e6c3ebbefe044
Rahul Patel <patelr4142@gmail.com>
2024-02-04 20:39:31 +0530

    updated lambda code to initialize count

--- lambda_functions/visit_counter.py
+++ lambda_functions/visit_counter.py
@@ -6,18 +6,43 @@ env = os.environ.get('environment_acronym')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(f'rahul-crc-use1-{env}-visit-counter-table')
 
-def lambda_handler(event, context):
-    response = table.get_item(Key= {'id' : 'count'} )
-    count = response["Item"]["visitor_count"]
+def initialize_count():
+    try:
+        # Try to get the 'count' item
+        table.get_item(Key={'id': 'count'})
+    except dynamodb.meta.client.exceptions.ResourceNotFoundException:
+        # 'count' item doesn't exist, initialize it with the initial count value
+        table.put_item(Item={'id': 'count', 'visitor_count': '1'})
 
-    new_count = str(int(count)+1)
+# Initialize the 'count' item if it doesn't exist
+initialize_count()
+
+def lambda_handler(event, context):
+    response = table.get_item(Key={'id': 'count'})
+    
+    if 'Item' not in response:
+        # 'count' item doesn't exist, initialize it
+        initialize_count()
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Item initialized with initial count'}),
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            },
+        }
+
+    count = response["Item"]["visitor_count"]
+    new_count = str(int(count) + 1)
+    
     response = table.update_item(
         Key={'id': 'count'},
         UpdateExpression='set visitor_count = :c',
         ExpressionAttributeValues={':c': new_count},
         ReturnValues='UPDATED_NEW'
-        )
-    
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps(response),
