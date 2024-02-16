70f711d680a49587098dbca8a50abcd6954c08eb
Rahul Patel <patelr4142@gmail.com>
2024-02-16 12:16:33 +0530

    modified route 53 for acm records to use data block

--- route53.tf
+++ route53.tf
@@ -49,32 +49,58 @@ resource "aws_route53_record" "SOARoute53Record" {
   records = [ "ns-23.awsdns-02.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400" ]
 }
 
+data "aws_acm_certificate" "my_certificate" {
+  domain = "rahulpatel.cloud"
+}
+
+data "aws_route53_zone" "route53_zone" {
+  name         = "rahulpatel.cloud"
+  private_zone = false
+}
+
 resource "aws_route53_record" "CNAME1Route53Record" {
-  zone_id = aws_route53_zone.primary.zone_id
+  for_each = {
+    for dvo in aws_acm_certificate.MyCertificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 300
+  type = each.value.type
+  zone_id = data.aws_route53_zone.route53_zone.zone_id
+  }
 
-  name    = "_7eaa71837c1bba978d1db63cfefd3a6c.rahulpatel.cloud"
-  type    = "CNAME"
-  ttl     = 300
 
-  records = [ "_0913b7dce3294c3a2bf69a3ffc087ac2.mhbtsbpdnt.acm-validations.aws." ]
-}
+# resource "aws_route53_record" "CNAME1Route53Record" {
+#   zone_id = aws_route53_zone.primary.zone_id
 
-resource "aws_route53_record" "CNAME2Route53Record" {
-  zone_id = aws_route53_zone.primary.zone_id
+#   name    = "_7eaa71837c1bba978d1db63cfefd3a6c.rahulpatel.cloud"
+#   type    = "CNAME"
+#   ttl     = 300
 
-  name    = "_82889e8f1686df7ce61de80772b0317c.aws.rahulpatel.cloud"
-  type    = "CNAME"
-  ttl     = 300
+#   records = [ "_0913b7dce3294c3a2bf69a3ffc087ac2.mhbtsbpdnt.acm-validations.aws." ]
+# }
 
-  records = [ "_e74127dc03a611f43187b2ee3e68ddce.mhbtsbpdnt.acm-validations.aws." ]
-}
+# resource "aws_route53_record" "CNAME2Route53Record" {
+#   zone_id = aws_route53_zone.primary.zone_id
 
-resource "aws_route53_record" "CNAME3Route53Record" {
-  zone_id = aws_route53_zone.primary.zone_id
+#   name    = "_82889e8f1686df7ce61de80772b0317c.aws.rahulpatel.cloud"
+#   type    = "CNAME"
+#   ttl     = 300
 
-  name    = "_92c48bfe319b92cdf250e2a707413c3a.www.rahulpatel.cloud"
-  type    = "CNAME"
-  ttl     = 300
+#   records = [ "_e74127dc03a611f43187b2ee3e68ddce.mhbtsbpdnt.acm-validations.aws." ]
+# }
 
-  records = [ "_bf570369bf6b23696db2ee26540e2495.mhbtsbpdnt.acm-validations.aws." ]
-}
+# resource "aws_route53_record" "CNAME3Route53Record" {
+#   zone_id = aws_route53_zone.primary.zone_id
+
+#   name    = "_92c48bfe319b92cdf250e2a707413c3a.www.rahulpatel.cloud"
+#   type    = "CNAME"
+#   ttl     = 300
+
+#   records = [ "_bf570369bf6b23696db2ee26540e2495.mhbtsbpdnt.acm-validations.aws." ]
+# }
